48f07cfc0181c079c5dae33a53d00ac7103a5222
marti <marti.piris@estudiantat.upc.edu>
2026-03-30 17:34:23 +0200

    Environment fix to work on local

--- src/issueTracker/settings.py
+++ src/issueTracker/settings.py
@@ -17,8 +17,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='dummy_key')
+DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
@@ -114,9 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
