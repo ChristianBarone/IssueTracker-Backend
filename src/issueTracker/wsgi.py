8f789999d43fbad3a6164e00f446b7748bc0f141
Martí Piris <marti.piris@estudiantat.upc.edu>
2026-05-21 13:52:29 +0200

    Revert connection pooling

--- src/issueTracker/wsgi.py
+++ src/issueTracker/wsgi.py
@@ -10,23 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.db import connections
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'issueTracker.settings')
-
-try:
-    from psycopg_pool import ConnectionPool
-    from django.conf import settings
-
-    db_config = settings.DATABASES['default']
-    conninfo = f"postgresql://{db_config['USER']}:{db_config['PASSWORD']}@{db_config['HOST']}:{db_config['PORT']}/{db_config['NAME']}"
-
-    pool = ConnectionPool(conninfo, min_size=10, max_size=20)
-
-    from psycopg import connect
-
-    connections.databases['default']['CONNECTION'] = pool.connection()
-except Exception as e:
-    print(f"Error configurando pool: {e}")
 
 application = get_wsgi_application()
